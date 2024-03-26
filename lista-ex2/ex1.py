57dcc5ab25238295556a25b318d64cd057404175
Guilherme Yoshida <126599207+gyyoshida@users.noreply.github.com>
2024-03-26 20:59:21 -0300

    Correção ex1

--- lista-ex2/ex1.py
+++ lista-ex2/ex1.py
@@ -1,11 +1,11 @@
 from random import randint
 
-random = randint(0, 100)
+random = randint(1, 100)
 palpite = 0;
 
 print("Iniciando o jogo de adivinhação!!!")
 while palpite != random:
-    palpite = int(input("Digite um número entra 0 a 100: "))
+    palpite = int(input("Digite um número entra 1 a 100: "))
     if palpite == random:
         print(f"Parabéns, você venceu! O número era {random}")
         break;
